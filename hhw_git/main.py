c44200f938a2f1a5c60ab87e661c271f088fcfca
m4rsm4ps <79999161+m4rsm4ps@users.noreply.github.com>
2021-12-02 22:35:10 +0200

    parse func modified tests added .gitignore is tracked now

--- hhw_git/main.py
+++ hhw_git/main.py
@@ -1,19 +1,30 @@
 def parse(query: str) -> dict:
     parsed = {}
-    if '?' in query and not query.endswith('?'):
-        toparse = query[query.find('?') + 1:]
-        items = toparse.strip('&').split('&')
-        for i in items:
-            key, val = i.split("=")
-            parsed[key] = val
-
+    query = query.partition('?')[-1]
+    for bits in query.split('&'):
+        try:
+            key, lol, val = bits.partition("=")
+        except ValueError:
+            continue
+        else:
+            if key and val: parsed[key] = val
     return parsed
 
 
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&&color=purple') == {'name': 'ferret', 'color': 'purple'}
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?me=f*rr*t=fe??et&color=?') == {'me': 'f*rr*t=fe??et', 'color': '?'}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?name=Dima==Dima') == {'name': 'Dima==Dima'}
+    assert parse('https://example.com/path/to/page??=?') == {'?': '?'}
+    assert parse('https://example.com/path/to/page?=========3') == {}
+    assert parse('https://example.com/path/to/page?name=') == {}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?=&=') == {}
+    assert parse('https://example.com/path/to/page?&&&&&&&&&&&&&&&&&&&&&&') == {}
+    assert parse('http://example.com/path/to/page?zd@røväzæbáł') == {}
+    assert parse('') == {}
 
