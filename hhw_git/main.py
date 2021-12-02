62ac216e928eabc6f3d06f1eeb04e2dd669d19fe
m4rsm4ps <79999161+m4rsm4ps@users.noreply.github.com>
2021-12-03 00:47:19 +0200

    main.py updated with parse_cookie func readme updated

--- hhw_git/main.py
+++ hhw_git/main.py
@@ -1,13 +1,15 @@
 def parse(query: str) -> dict:
-    parsed = {}
+    parsed = dict()
     query = query.partition('?')[-1]
     for bits in query.split('&'):
         try:
-            key, lol, val = bits.partition("=")
+            key, val = bits.partition('=')[0], bits.partition('=')[-1]
         except ValueError:
             continue
         else:
-            if key and val: parsed[key] = val
+            if key and val:
+                parsed[key] = val
+
     return parsed
 
 
@@ -27,3 +29,34 @@ if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?&&&&&&&&&&&&&&&&&&&&&&') == {}
     assert parse('http://example.com/path/to/page?zd@røväzæbáł') == {}
     assert parse('') == {}
+
+
+def parse_cookie(query: str) -> dict:
+    parsed = dict()
+    for bit in query.split(';'):
+        try:
+            key, val = bit.partition('=')[0], bit.partition('=')[-1]
+        except ValueError:
+            continue
+        else:
+            if key and val:
+                parsed[key] = val
+
+    return parsed
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=Dima=User') == {'name': 'Dima=User'}
+    assert parse_cookie('hello') == {}
+    assert parse_cookie('name;age') == {}
+    assert parse_cookie(';') == {}
+    assert parse_cookie('=') == {}
+    assert parse_cookie('=????;****') == {}
+    assert parse_cookie('????=****') == {'????': '****'}
+    assert parse_cookie(';Dima=name;;;;;') == {'Dima': 'name'}
+    assert parse_cookie('0==0;1;;1') == {'0': '=0'}
+    assert parse_cookie('=;=;=;=;=;spam=eggs') == {'spam': 'eggs'}
